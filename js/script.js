885a3452a6e62053124486ec7adedfe4f08c9020
Tom Dunstan <github@tomd.cc>
2011-05-03 12:58:17 +0100

    add comments to post page

--- js/script.js
+++ js/script.js
@@ -1,26 +1,8 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ var disqus_shortname = 'tomdcc';
+$(document).ready(function() {
+    if($('#disqus_thread').length > 0) {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    }
+});
